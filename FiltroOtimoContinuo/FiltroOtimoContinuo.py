6335d6fea403086213aa952236c74edd175c5c08
DiogoCardinot <diogocardinotuerj@gmail.com>
2024-05-02 21:24:00 -0300

    Precisa dar um jeito de salvar cada dado em uma linha diferente do excel de acordo com a ocupacao

--- FiltroOtimoContinuo/FiltroOtimoContinuo.py
+++ FiltroOtimoContinuo/FiltroOtimoContinuo.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Número do janelamento desejado
 n_janelamento = 7
 pedestal = 30
-ocupacao = 20
+ocupacao = 30
 
 ############################################### CARREGAR INFORMAÇÕES DO PULSO DE REFERÊNCIA E DERIVADA ##################################################
 nome_arquivo_saida = "C:/Users/diogo/Desktop/Diogo(Estudos)/Mestrado/TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias1/FiltroOtimoContinuo/valores_g_derivada.txt"
@@ -172,13 +172,13 @@ desvioPadraoErroEstimacao = np.std(erroEstimacaoAmplitude)
 ############################################# SALVAR AS INFORMAÇÕES RELEVANTES EM UM TXT #############################################################
 
 # Caminho do arquivo onde os dados do pulso e sua referência serão salvos
-nome_arquivo_saida = "C:/Users/diogo/Desktop/Diogo(Estudos)/Mestrado/TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias1/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacao_"+str(ocupacao)+".txt"
+# nome_arquivo_saida = "C:/Users/diogo/Desktop/Diogo(Estudos)/Mestrado/TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias1/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacao_J"+str(n_janelamento)+".txt"
 
 # notebook
 # nome_arquivo_saida = "C:\Users\diogo\OneDrive\Área de Trabalho\TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias11/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacao_"+str(ocupacao)+".txt"
 
 # Definir os títulos das colunas
-titulos = ['Janelamento', 'Pesos', 'Matriz_Covariancia', 'Erro_Estimacao_Amplitude', 'Media_Erro_Estimacao', 'Desvio_Padrao_Erro_Estimacao']
+titulos = ['Ocupacao', 'Pesos', 'Matriz_Covariancia', 'Erro_Estimacao_Amplitude', 'Media_Erro_Estimacao', 'Desvio_Padrao_Erro_Estimacao']
 
 # # Verificar se o arquivo existe, e se não, criar
 # if not os.path.exists(nome_arquivo_saida):
@@ -210,7 +210,7 @@ titulos = ['Janelamento', 'Pesos', 'Matriz_Covariancia', 'Erro_Estimacao_Amplitu
 
 # Criar um DataFrame com os seus dados
 dados_dict = {
-    'Janelamento': [n_janelamento],
+    'Ocupacao': [ocupacao],
     'Pesos': [w],
     'Matriz_Covariancia': [cov_matrix_ruido],
     'Erro_Estimacao_Amplitude': [erroEstimacaoAmplitude],
@@ -220,28 +220,28 @@ dados_dict = {
 dados_df = pd.DataFrame(dados_dict)
 
 # Caminho do arquivo Excel de saída
-# caminho_arquivo_excel = "C:/Users/diogo/Desktop/Diogo(Estudos)/Mestrado/TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias1/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacao_"+str(ocupacao)+".xlsx"
+caminho_arquivo_excel = "C:/Users/diogo/Desktop/Diogo(Estudos)/Mestrado/TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias1/FiltroOtimoContinuo/ErroEstimacao/ErroEstimacao_J"+str(n_janelamento)+".xlsx"
 
 # notebook
-caminho_arquivo_excel = "C:\Users\diogo\OneDrive\Área de Trabalho\TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias11/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacao_"+str(ocupacao)+".xlsx"
+# caminho_arquivo_excel = "C:\Users\diogo\OneDrive\Área de Trabalho\TEMC-Processamento-Avan-ado-de-Dados-para-Calorimetria-de-Altas-Energias11/FiltroOtimoContinuo/ErrosEstimacao/ErroEstimacaoJ_"+str(n_janelamento)+".xlsx"
 # Verificar se o arquivo Excel já existe
 arquivo_existe = os.path.exists(caminho_arquivo_excel)
 
 # Salvar os dados no arquivo Excel
-with pd.ExcelWriter(caminho_arquivo_excel, mode='a' if arquivo_existe else 'w', engine='openpyxl', if_sheet_exists='replace') as writer:
+with pd.ExcelWriter(caminho_arquivo_excel, mode='a' if arquivo_existe else 'w', engine='openpyxl') as writer:
     if arquivo_existe:
         dados_existente = pd.read_excel(caminho_arquivo_excel)
         # Verificar se já existe uma linha com o número do janelamento atual
-        linha_existente_index = dados_existente[dados_existente['Janelamento'] == n_janelamento].index
+        linha_existente_index = dados_existente[dados_existente['Ocupacao'] == ocupacao].index
         # Se a linha existir, sobrescrever com os novos valores
         if not linha_existente_index.empty:
             dados_existente.loc[linha_existente_index] = dados_df.values
             dados_existente.to_excel(writer, index=False, header=True, sheet_name='Dados')
-            print(f"Dados atualizados para janelamento {n_janelamento}")
+            print(f"Dados atualizados para ocupacao {ocupacao}")
         # Se a linha não existir, adicionar como uma nova linha no final do arquivo
         else:
             dados_df.to_excel(writer, index=False, header=not arquivo_existe, sheet_name='Dados', startrow=len(dados_existente) + 1)
-            print(f"Dados adicionados para janelamento {n_janelamento}")
+            print(f"Dados adicionados para ocupacao {ocupacao}")
     else:
         dados_df.to_excel(writer, index=False, header=not arquivo_existe, sheet_name='Dados')
         print("Dados salvos no arquivo Excel.")
